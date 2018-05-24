b0f7c52f610c165fbf8b3856f5cc56ad6c738a47
Pouria Mellati <pouria@pouria.me>
2018-05-25 00:02:58 +1000

    Callback based sbt execution.

--- timer.py
+++ timer.py
@@ -1,7 +1,13 @@
+#!/usr/bin/env python3
+
 from subprocess import run
 import subprocess
 import argparse
 import re
+import asyncio
+import os
+
+# SEE: https://stackoverflow.com/questions/375427/non-blocking-read-on-a-subprocess-pipe-in-python/437888
 
 def escape_ansi(line):
     ansi_escape = re.compile(r'(\x9B|\x1B\[)[0-?]*[ -/]*[@-~]')
@@ -21,9 +27,22 @@ scala_files = list(filter(
 
 print('Scala files\n' + str(len(scala_files)))
 
-sbt = subprocess.Popen(['sbt'], cwd = project_root, stdin = subprocess.PIPE, stdout = subprocess.PIPE)
+class SubprocessProtocol(asyncio.SubprocessProtocol):
+    def pipe_data_received(self, fd, data):
+        if fd == 1: # got stdout data (bytes)
+            print('GOT STDOUT:')
+            print(escape_ansi(data.decode()))
 
-line = escape_ansi(sbt.stdout.readline().decode())
+    def connection_lost(self, exc):
+        loop.stop() # end loop.run_forever()
 
-print('SBT LINES:\n' + line[0:6])
-print(len(line))
+if os.name == 'nt':
+    loop = asyncio.ProactorEventLoop() # for subprocess' pipes on Windows
+    asyncio.set_event_loop(loop)
+else:
+    loop = asyncio.get_event_loop()
+try:
+    loop.run_until_complete(loop.subprocess_exec(SubprocessProtocol, 'sbt', cwd = project_root))
+    loop.run_forever()
+finally:
+    loop.close()
